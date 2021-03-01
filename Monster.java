b2b7233efe120fff1c63b9725c345227de36d0c8
Henry-13 <77869605+Henry-13@users.noreply.github.com>
2021-03-01 17:07:37 +0800

    Add files via upload

---
+++ Monster.java
@@ -0,0 +1,11 @@
+public class Monster {
+    private String name;
+    private int life;
+    private int attack;
+
+    public Monster(String name,int life,int attack) {
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+    }
+}
