125c54ba60ee6383391249ee4155ed7f224bd61f
Henry-13 <77869605+Henry-13@users.noreply.github.com>
2021-03-01 17:14:16 +0800

    Add files via upload

---
+++ second/Human.java
@@ -0,0 +1,14 @@
+ public class Human {
+    private String name;
+    private int life;
+    private int attack;
+    private int defense;
+
+    public Human(String name,int attack,int life,int defense) {
+        this.name = name;
+        this.attack = attack;
+        this.life = life;
+        this.defense = defense;
+    }
+
+}
